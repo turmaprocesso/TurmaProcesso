1e1a48972909ac892b28ec7229c5c75bec912f78
turmaprocesso <fercrescencio@hotmail.com>
2015-08-22 11:41:06 -0300

    Criacao projeto
    Criacao Projeto

---
+++ ProjetoEclipse/ProjetoShoppin/gen/turmaprocesso/projetoshoppin/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package turmaprocesso.projetoshoppin;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ProjetoEclipse/ProjetoShoppin/src/turmaprocesso/projetoshoppin/Principal.java
@@ -0,0 +1,15 @@
+package turmaprocesso.projetoshoppin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Principal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.entrada);
+	}
+}
