d1b44a12145bc66fcf8df1df8494111fe330ba03
turmaprocesso <fercescencio@hotmail.com>
2015-09-05 01:43:23 -0300

    Grupo Futuros Compradores: Criação da Tela Inicio
    Criação do layout para a tela Inicio do cadastro de futuros compradores.

--- ProjetoEclipse/ProjetoShoppin/gen/turmaprocesso/projetoshoppin/R.java
+++ ProjetoEclipse/ProjetoShoppin/gen/turmaprocesso/projetoshoppin/R.java
@@ -13,12 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f050000;
+        public static final int button2=0x7f050002;
+        public static final int textView1=0x7f050001;
+    }
     public static final class layout {
         public static final int entrada=0x7f030000;
         public static final int grupofuturocadastrocompradores=0x7f030001;
         public static final int grupotelaalugueis=0x7f030002;
     }
     public static final class string {
+        public static final int Botão=0x7f040003;
+        public static final int BotãoCadastrar=0x7f040005;
+        public static final int BotãoCancelar=0x7f040004;
+        public static final int Titulo=0x7f040002;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
     }
